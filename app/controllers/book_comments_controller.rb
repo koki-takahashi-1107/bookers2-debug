881c7e83e37f8a140a8cf1c8636a64d2c069c685
koki-takahashi-1107 <jubeatknit18@gmail.com>
2021-04-03 07:58:53 +0000

    Update2 Commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,16 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
+  
   def create
-   book = Book.find(params[:book_id])
-   comment = current_user.book_comments.new(book_comment_params)
-   comment.book_id = book.id
-   comment.save
-   redirect_to book_path(book)
+   @book = Book.find(params[:book_id])
+   @book_comment = current_user.book_comments.new(book_comment_params)
+   @book_comment.book_id = book.id
+   @book_comment.user_id =current_user.id
+   if @book_comment.save
+   redirect_to book_path(@book.id)
+   else
+     render 'books/show'
+   end
   end
 
   def destroy
