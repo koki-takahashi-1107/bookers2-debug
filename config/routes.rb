f0f26d7ab46389416cbf439c1d9fca35cdcabd25
koki-takahashi-1107 <jubeatknit18@gmail.com>
2021-04-07 05:45:44 +0000

    Update4 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-   root 'homes#top'
+  root 'homes#top'
   get 'home/about' => 'homes#about'
 
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
- end
-  resources :users,only: [:show,:index,:edit,:update] do
-     resource :relationships, only: [:create, :destroy]
   end
-
-  resources :users do
+  resources :users,only: [:show,:index,:edit,:update] do
     member do
       get :following, :followers
     end
+    resource :relationships, only: [:create, :destroy]
   end
 
   post '/favorite/:id' => 'favorites#favorite', as: 'favorite'
-  post '/cosplay_favorite/:id' => 'cosplay_favorites#favorite', as: 'cosplay_favorite'
 
+  get '/search' => 'search#search'
 end
