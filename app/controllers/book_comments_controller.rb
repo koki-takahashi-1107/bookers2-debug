6cbe701ad8979233895d5052165c7f7d6d14b0ec
koki-takahashi-1107 <jubeatknit18@gmail.com>
2021-04-10 06:20:31 +0000

    Ajax commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,28 +1,24 @@
 class BookCommentsController < ApplicationController
-  before_action :authenticate_user!
+ before_action :authenticate_user!
 
   def create
-   @book = Book.find(params[:book_id])
-   @book_comment = BookComment.new(book_comment_params)
-   @book_comment.book_id = book.id
-   @book_comment.user_id =current_user.id
-   if @book_comment.save
-   redirect_to book_path(@book.id)
-   else
-     render 'books/show'
-   end
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    unless @book_comment.save
+      render 'error' 
+    end
   end
 
   def destroy
-   BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-   redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
   end
-
-   private
-
+  
+  private
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-
-
 end
